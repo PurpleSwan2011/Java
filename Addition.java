71edd27eedeeeec92faf1d6bbd1643b733d273ed
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-06-10 16:51:07 +0530

    s

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+    public static void main(String[]args)
+    {
+        int a=20;
+        int b=60;
+        int s=a+b;
+        System.out.println(a + "+" + b + "=" + s);
+    }
+}
