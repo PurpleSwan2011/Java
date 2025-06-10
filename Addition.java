10bf1a1a7325d0e4f85da8b3a75b4d499452a5c7
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-06-10 16:58:32 +0530

    d

--- Addition.java
+++ Addition.java
@@ -5,6 +5,12 @@ public class Addition
         int a=20;
         int b=60;
         int s=a+b;
+        int m=a*b;
+        int l=a-b;
+        int p=a/b;
         System.out.println(a + "+" + b + "=" + s);
+        System.out.println(a + "*" + b + "=" + m);
+        System.out.println(a + "-" + b + "=" + l);
+        System.out.println(a + "/" + b + "=" + p);
     }
 }
