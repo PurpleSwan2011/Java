7d032f525e5c92a5888080c19db20cd4a9300888
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-06-21 11:15:26 +0530

    h

---
+++ Hellouser.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hellouser {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("pls enter:");
+        String name=sc.nextLine();
+        System.out.println("enter");
+        int lucky=sc.nextInt();
+        System.out.println("Hello"+name+",your lucky number"+lucky+"shows ur really harworking");
+    }
+}
