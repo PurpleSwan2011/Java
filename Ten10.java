eb380787007a02a219b3d5ff12e961d763c85374
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-06-21 11:03:06 +0530

    f

---
+++ Ten10.java
@@ -0,0 +1,12 @@
+public class Ten10 {
+    public static void main(String[]args){
+        for(int i=10;i>1;i--){
+            System.out.println("the value of i is:" +i);
+    }
+    int j=0;
+    while(j<11){
+        System.out.println("the value of i is:"+j);
+        j++;
+    }
+}
+}
