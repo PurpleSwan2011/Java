ba5318f7bb7fd8ac5a1ed48cc12d7a896fc0c15c
Saima Fathima <saima.fathi2011@gmail.com>
2025-07-01 20:45:12 +0530

    1

---
+++ Sorting.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Sorting {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the size of the array
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input the array elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Bubble Sort Algorithm
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Output the sorted array
+        System.out.println("Sorted array in ascending order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
